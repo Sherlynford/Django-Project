c5b2078477fed0251adbc3ad568a3b998eb6c475
Sherlynford <bobowba2547@gmail.com>
2023-09-16 21:54:20 +0700

    Done

---
+++ myapp/members/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import loader
+from .models import Members
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('first.html')
+    context = {
+        'mymembers' : mymembers
+    }
+    return HttpResponse(template.render(context,request))
+def add (request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({},request))
+
+def addrecord(request):
+    firstname = request.POST['first']
+    lastname = request.POST['last']
+    member = Members(firstname = firstname, lastname = lastname )
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def delete(request,id):
+    member = Members.objects.get(id = id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index'))
+def update(request,id):
+    mymembers = Members.objects.get(id = id)
+    template = loader.get_template('update.html')
+    context = {
+        'mymembers' : mymembers
+    }
+    return HttpResponse(template.render(context, request))
+def updaterecord(request,id):
+    firstname = request.POST['first']
+    lastname = request.POST['last']
+    member = Members.objects.get(id = id)
+    member.firstname = firstname
+    member.lastname = lastname
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
